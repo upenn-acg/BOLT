e856fc6a24fbdc47d6b691b32adbcd2ce7b40a52
zyuxuan0115 <kate0115.zyx@gmail.com>
2023-06-09 19:50:38 -0400

    update

--- bolt/lib/Rewrite/BinaryPassManager.cpp
+++ bolt/lib/Rewrite/BinaryPassManager.cpp
@@ -483,7 +483,7 @@ void BinaryFunctionPassManager::runAllPasses(BinaryContext &BC) {
   Manager.registerPass(std::make_unique<LowerAnnotations>(NeverPrint));
 */
   //Manager.registerPass(std::make_unique<InjectPrefetchPass>());
-//  Manager.registerPass(std::make_unique<InjectPrefetchLitePass>());
+  //Manager.registerPass(std::make_unique<InjectPrefetchLitePass>());
   Manager.registerPass(std::make_unique<InjectPrefetchOuterLoopPass>());
 
 
